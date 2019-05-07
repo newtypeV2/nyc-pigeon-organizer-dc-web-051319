e6fad219e5f8abc84ce9dc6b3df4d330aed3341b
Gian Libunao <gianlibunao@gmail.com>
2019-05-07 20:36:37 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,5 +3,16 @@ require "pry"
 def nyc_pigeon_organizer(data)
   # write your code here!
   pigeonSorted = {}
-  binding.pry
+  data.each{|attribute,pdata| 
+	pdata.each{|info,names| 
+		names.each{|name| 
+		binding.pry
+			pigeonSorted.keys.include?(name) ? 
+				pigeonSorted[name].keys.include?(attribute) ? 
+					pigeonSorted.merge(pigeonSorted[name]={attribute => [info]}) : pigeonSorted[name]={attribute => [] << info} 
+				: pigeonSorted[name]={attribute => [info]} 
+		}
+	}
+}
+  
 end
