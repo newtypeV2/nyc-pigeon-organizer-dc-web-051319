8edb4184046323d15b2ede4070af4fa3cc289b91
Gian Libunao <gianlibunao@gmail.com>
2019-05-07 21:46:54 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,7 +8,7 @@ def nyc_pigeon_organizer(data)
 data.collect{|attribute,pdata| 
 	pdata.collect{|info,names| 
 		names.each{|name| 
-			pigeonSorted.keys.include?(name) ? pigeonSorted[name].keys.include?(attribute) ? pigeonSorted[name][attribute] << info.to_s : pigeonSorted[name].merge({attribute=>[info.to.s]}) : pigeonSorted[name]={attribute => [info.to_s]}
+			pigeonSorted.keys.include?(name) ? pigeonSorted[name].keys.include?(attribute.to_s) ? pigeonSorted[name][attribute.to_s] << info.to_s : pigeonSorted[name].merge({attribute.to_s=>[info.to.s]}) : pigeonSorted[name]={attribute.to_s => [info.to_s]}
 			binding.pry
 		}
 	}
