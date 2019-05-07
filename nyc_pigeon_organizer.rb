a566909af71ebbb0cd8d4478898ab4104e5d5d85
Gian Libunao <gianlibunao@gmail.com>
2019-05-07 21:53:41 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,6 +4,8 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   pigeonSorted = {}
   
+pigeonSorted.keys.include?(name) ? pigeonSorted[name] : pigeonSorted[name]={attribute => [] << info.to_s}
+binding.pry
   
 
 end
